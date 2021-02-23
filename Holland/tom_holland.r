73936ddc6f6fd37d460be1717518c8be48edda92
Goodernews <tbogoodnews@gmail.com>
2021-02-23 11:58:45 -0800

    Updated

--- Holland/tom_holland.r
+++ Holland/tom_holland.r
@@ -1,3 +1,11 @@
+library(tidyverse)
+library(ggrepel)
+
+
+# Initially made in September 2020 when I was learning R.
+# Data was hand scrapped from Rotten Tomatoes, Wikipedia, and IMDB
+
+
 holland <- data.frame(
   movie = c("The Impossible", 
             "How I Live Now",
@@ -13,7 +21,7 @@ holland <- data.frame(
             "Dolittle",
             "Onward",
             "In the Heart of the Sea"),
-  rotten_tomatoes = c(81, 66, 91, 91, 86, 92, 85, 94, 90, 61, 75, 14, 88, 43),
+  rotten_tomatoes = c(0.81, 0.66, 0.91, 0.91, 0.86, 0.92, 0.85, 0.94, 0.90, 0.61, 0.75, 0.14, 0.88, 0.43),
   box_office = c(19, 0.06, 1.4, 408.1, 8.6, 334.2, 665, 2798, 1132, 12.3, 171.6, 249.7, 135.5, 23.06),
   Accent = c("British", "British", "British", "American", "British", "American", 
              "American", "American", "American", "British", "American", "British", "American", "American")
@@ -21,4 +29,9 @@ holland <- data.frame(
 
 ggplot(data=holland, mapping = aes(rotten_tomatoes, box_office, color=Accent)) + 
   geom_point() +
-    scale_colour_manual(values=c("blue", "red"))
+  geom_text_repel(aes(label = movie), size = 4) +
+  xlab("Rotten Tomato Score") +
+  ylab("Box Office (Millions)")+
+  ggtitle("Tom Holland's Accent")+
+  scale_colour_manual(values=c("blue", "red")) +
+  scale_x_continuous(labels = scales::percent)
